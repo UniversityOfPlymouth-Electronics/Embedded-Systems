8c357098b40debd1a3786db474c7250d395c6256
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-10-13 12:01:27 +0100

    Task112 - getchar and scanf example added

---
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -0,0 +1,46 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+Buzzer buzz;
+
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+
+
+int main()
+{
+    printf("\nTASK-112\n");
+
+    //getchar
+    char c1, c2;
+    printf("\n\nPress a key\n");
+    c1 = getchar();
+    printf("You entered character %c which has the ASCII code %d\n", c1, c1);
+
+    printf("\n\nPress another key\n");
+    c2 = getchar();
+    printf("You entered character %c which has the ASCII code %d\n", c2, c2);
+
+    //scanf
+    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
+    int delay_ms;
+    int parsed = scanf("%d", &delay_ms);
+    
+    printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
+
+    // Stop
+    while (parsed > 0) {
+        wait_us(delay_ms * 1000);
+        led1 = 1;
+        led2 = 1;
+        led3 = 1;
+        wait_us(delay_ms * 1000);
+        led1 = 0;
+        led2 = 0;
+        led3 = 0;        
+    } 
+
+    printf("Invalid - please restart and try again\n");
+    while(1);    
+}
--- level4/TASK112.md
+++ level4/TASK112.md
@@ -3,8 +3,22 @@
 ---
 
 # Variables and Terminal Input
+We saw how data can written to a terminal, but what about reading?
 
-[ COMING SOON ]
+For this, we have a few methods but we will focus on just two of them:
+
+* `getchar`
+* `scanf`
+
+## Reading a single character with `getchar`
+
+
+# Quiz
+Please attempt the following Quiz before proceeding.
+
+[Link to Quiz on the DLE](https://dle.plymouth.ac.uk/mod/quiz/view.php?id=977697)
+
+Thank you.
 
 ---
 
