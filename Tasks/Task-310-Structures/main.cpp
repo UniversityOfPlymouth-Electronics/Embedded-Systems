5e8b865592bf65b37e2d10b6fa06d302464d204a
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-10-15 17:45:02 +0100

    Started structures and Classes

---
+++ Tasks/Task-310-Structures/main.cpp
@@ -0,0 +1,123 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+#include <cstring>
+using namespace uop_msb_200;
+
+// Inputs
+DigitalIn SWB(USER_BUTTON); // THIS IS THE BLUE BUTTON
+DigitalIn SW2(BTN1_PIN);    // SW2
+DigitalIn SW3(BTN2_PIN);    // SW3
+
+// Outputs
+DigitalOut ledRed(TRAF_RED1_PIN);
+DigitalOut ledYel(TRAF_YEL1_PIN);
+DigitalOut ledGrn(TRAF_GRN1_PIN);
+
+// Timers (modified version from Timer)
+TimerCompat tmr_debounce2;
+TimerCompat tmr_debounce3;
+TimerCompat tmr_flash;
+
+// Switch states
+typedef enum {
+  WAIT_FOR_PRESS,
+  WAITING_1,
+  WAIT_FOR_REL,
+  WAITING_2
+} SWITCH_STATE;
+
+SWITCH_STATE sw_state2 = WAIT_FOR_PRESS;
+SWITCH_STATE sw_state3 = WAIT_FOR_PRESS;
+
+int main() {
+  // Start flashing timer
+  tmr_flash.start();
+
+  while (true) {
+
+    // READ INPUTS **WITHOUT** BLOCKING
+    long long flash_time = tmr_flash.read_ms();
+    int sw2 = SW2.read();
+    int sw3 = SW3.read();
+    long long sw2_time = tmr_debounce2.read_ms();
+    long long sw3_time = tmr_debounce3.read_ms();
+
+    // Update yellow LED state and mealy outputs
+    if (flash_time >= 500) {
+      ledYel = !ledYel;
+      tmr_flash.reset();
+    }
+
+    // Update switch2 state machine and mealy outputs
+    switch (sw_state2) {
+
+    case WAIT_FOR_PRESS:
+      if (sw2 == 1) {
+        sw_state2 = WAITING_1;
+        tmr_debounce2.start();
+        ledGrn = !ledGrn;
+      }
+      break;
+    case WAITING_1:
+      if (sw2_time >= 300) {
+        sw_state2 = WAIT_FOR_REL;
+        tmr_debounce2.stop();
+        tmr_debounce2.reset();
+      }
+      break;
+
+    case WAIT_FOR_REL:
+      if (sw2 == 0) {
+        sw_state2 = WAITING_2;
+        tmr_debounce2.start();
+      }
+      break;
+    case WAITING_2:
+      if (sw2_time >= 300) {
+        sw_state2 = WAIT_FOR_PRESS;
+        tmr_debounce2.stop();
+        tmr_debounce2.reset();
+      }
+      break;
+
+    default:
+      sw_state2 = WAIT_FOR_PRESS;
+    }
+
+    // Update switch3 state machine and mealy outputs
+    // VERY REPETETIVE CODE!
+    switch (sw_state3) {
+
+    case WAIT_FOR_PRESS:
+      if (sw3 == 1) {
+        sw_state3 = WAITING_1;
+        tmr_debounce3.start();
+        ledRed = !ledRed;
+      }
+      break;
+    case WAITING_1:
+      if (sw3_time >= 300) {
+        sw_state3 = WAIT_FOR_REL;
+        tmr_debounce3.stop();
+        tmr_debounce3.reset();
+      }
+      break;
+
+    case WAIT_FOR_REL:
+      if (sw3 == 0) {
+        sw_state3 = WAITING_2;
+        tmr_debounce3.start();
+      }
+      break;
+    case WAITING_2:
+      if (sw3_time >= 300) {
+        sw_state3 = WAIT_FOR_PRESS;
+        tmr_debounce3.stop();
+        tmr_debounce3.reset();
+      }
+      break;
+
+    default:
+      sw_state3 = WAIT_FOR_PRESS;
+    }
+  }
+}
--- level6/Cplusplus.md
+++ level6/Cplusplus.md
@@ -3,10 +3,38 @@
 ---
 
 # C++ and Object Orientated Programming 
-In this 2-part lab, we will explore object orientated techniques in C++
+In this 2-part lab, we will explore object orientated techniques in C++. We begin with structures, which feature in both C and C++. Then we move to classes, which feature in C++ and are one of the main differentiating features between the two languages.  
+
+C++ is an "Object Orientated Language", whereas C is procedural. You can think of C++ as an extension of the C language, inheriting most of it's attributes and syntax.
+
+Central to this is the "Class". To understand Classes, we need to remind ourselves about structures.
+
+## Structures
+C and C++ both have structures. These are extremely useful for packaging data into a single object.
 
 
+| **TASK 310** | Structures |
+| --- | --- |
+| 1. | Set Task-310 as your active project |
 
+### Dots, Arrows, Value and Reference Semantics
+This all sounds confusing I appreciate!
+
+### Passing by value and reference
+
+### Writing an API
+
+## Enumerated Types
+
+## Two-Switch Problem as a FSM
+
+## Classes
+
+### Creating a Simple Class
+
+### References and Pointers
+
+### Simple Device Driver
 
 ---
 
