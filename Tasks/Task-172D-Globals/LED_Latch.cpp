b7b0e9fcdfc90f684e86027e13dceec24e535041
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-12-01 16:08:09 +0000

    global functions

---
+++ Tasks/Task-172D-Globals/LED_Latch.cpp
@@ -0,0 +1,111 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+// LED Bar Display - all the hardware bits
+BusOut dataBits(LED_D0_PIN, LED_D1_PIN, LED_D2_PIN, LED_D3_PIN, LED_D4_PIN, LED_D5_PIN, LED_D6_PIN, LED_D7_PIN);
+DigitalOut LED_BAR_OE(LED_BAR_OE_PIN,1);
+DigitalOut LED_RED_LE(LED_RED_LE_PIN);
+DigitalOut LED_GRN_LE(LED_GRN_LE_PIN);
+DigitalOut LED_BLUE_LE(LED_BLUE_LE_PIN);
+
+// Custom type for the Latch Enable (ON or OFF)
+typedef enum {OFF=0, ON=1} LATCH_ENABLE_STATE;
+
+// *********************
+// FUNCTION DECLARATIONS
+// *********************
+
+// Set the state of all latch-enable pins
+// u is either ON or OFF
+void setAllLatches(LATCH_ENABLE_STATE u);
+
+//Turn LED display on or off
+// on should be true to turn the display on 
+void setLEDDisplayON(bool on);
+
+//Initialise the LED display
+// dat is the initial data (ready for then the LEDs are enabled)
+// enabled should be true to switch them on when initialised
+void led_init(uint8_t dat, bool enabled);
+
+//Set value of the output and latch
+// dat is the 8-bit value to set
+// col is the colour: 'r' for red, 'y' for yellow and 'g' for green
+void setLatch(uint8_t dat, char col);
+
+// ********************
+// FUNCTION DEFINITIONS
+// ********************
+
+void setLatch(uint8_t dat, char col)
+{
+    //For safety
+    setAllLatches(OFF);
+
+    //Now follow the sequence
+    wait_us(1);
+    dataBits = dat;    //Set the 8-bit data pattern
+    wait_us(1);
+
+    //Depending on the chosen colour
+    switch (col)
+    {
+        case 'r':
+            LED_RED_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_RED_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+        case 'g':
+            LED_GRN_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_GRN_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+        case 'b':
+            LED_BLUE_LE  = 1;    //Copy dataBits to red latch outputs
+            wait_us(1);         
+            LED_BLUE_LE  = 0;    //Ignore inputs (dataBits)
+            break;
+    }
+    wait_us(1); 
+}
+
+void led_init(uint8_t dat, bool enabled=true)
+{
+    //Disable the LED outputs
+    setLEDDisplayON(OFF);
+
+    //Set data to initial value
+    dataBits = dat;
+
+    //Give a little time
+    wait_us(1);
+
+    //Enable all latches
+    setAllLatches(ON);
+
+    //Give a little time
+    wait_us(1);
+
+    //Disable all latches
+    setAllLatches(OFF);
+
+    if (enabled) {
+        setLEDDisplayON(ON);
+    }
+}
+
+void setAllLatches(LATCH_ENABLE_STATE u)
+{
+    LED_RED_LE = u;
+    LED_GRN_LE = u;
+    LED_BLUE_LE = u;
+}
+
+void setLEDDisplayON(bool on)
+{
+    if (on == true) {
+        LED_BAR_OE = 0;
+    } else {
+        LED_BAR_OE = 1;
+    }
+}
---
+++ Tasks/Task-172D-Globals/main.cpp
@@ -0,0 +1,80 @@
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
+using namespace uop_msb_200;
+
+AnalogIn pot(AN_POT_PIN);
+DigitalOut dispBackLight(LCD_BKL_PIN,1);
+LCD_16X2_DISPLAY disp;
+
+extern void setLatch(uint8_t dat, char col);
+extern void led_init(uint8_t dat, bool enabled=true);
+extern void setLEDDisplayON(bool on);
+
+int getAverageDelay(double alpha);
+int getDelayMS();
+
+uint8_t pattern_red[] =  {0,   1,  2,  4,  8,  16, 32, 64, 128,  0xFF};
+uint8_t pattern_grn[] =  {0,   3,  6,  12, 24, 48, 96, 192, 128, 0xFF};
+uint8_t pattern_blue[] = {128, 64, 32, 16, 8,  4,  2,  1,  0,    0xFF};
+uint8_t idx = 0;
+
+
+int main()
+{
+    printf("Functions demo\n");
+    
+    led_init(0x00);
+
+    while(true) {
+        
+        //Get average delay value
+        int delay_ms = getAverageDelay(0.95); 
+
+        //Update display
+        disp.cls();
+        disp.printf("Delay: %dms", delay_ms);
+
+        //Set the patterns
+        setLatch(pattern_red[idx],  'r');
+        setLatch(pattern_grn[idx],  'g');
+        setLatch(pattern_blue[idx], 'b');
+        idx++;
+        if (idx == 10) {
+            idx = 0;
+        }
+
+        //Turn LEDs ON
+        setLEDDisplayON(true);
+
+        //Wait
+        wait_us(delay_ms*1000);
+
+        //Turn LEDs OFF
+        setLEDDisplayON(false);
+
+        //Wait
+        wait_us(delay_ms*1000);            
+    }
+
+}
+
+int getDelayMS() 
+{
+    float p = pot; // 0 ... 1
+    int delay = 100 + 900*p;    //100...1000ms
+    return delay;
+}
+
+
+//Calculate average delay by filtering the potentiometer value
+int getAverageDelay(double alpha)
+{
+    static double meanPotValue = (double)getDelayMS();  //Mean pot value
+
+    for (unsigned int n=0; n<32; n++) {
+        int potValue = getDelayMS();                    //Get raw value (with noise)
+        meanPotValue = alpha*meanPotValue + (1.0-alpha)*potValue; //Handy forumula!
+    }
+    return (int)meanPotValue;
+}
+
+
