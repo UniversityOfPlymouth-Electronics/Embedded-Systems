ed5f0942e8e263a2d92fb8d4082c9da8c4ab210f
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-09-21 09:27:23 +0100

    Smart pointer demo - not finsihed

---
+++ Tasks/year3-demo-workspace/SmartPointers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+// https://learn.microsoft.com/en-us/cpp/cpp/smart-pointers-modern-cpp?view=msvc-170
+// https://learn.microsoft.com/en-us/cpp/standard-library/memory?view=msvc-170
+
+/*
+ "In modern C++, raw pointers are only used in small code blocks of limited scope, loops,
+ or helper functions where performance is critical and there is no chance of confusion about ownership."
+ https://learn.microsoft.com/en-us/cpp/cpp/smart-pointers-modern-cpp?view=msvc-170
+
+*/
+
+int main()
+{
+    cout << "Smart Pointer Demo" << endl;
+
+
+
+    return 0;
+}
