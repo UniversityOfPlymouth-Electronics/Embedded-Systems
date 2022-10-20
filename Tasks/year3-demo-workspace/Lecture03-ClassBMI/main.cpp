482e16e6c41186b6c4a3a0d889422f1c04ba8edd
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-10-20 18:00:21 +0100

    new materials

--- Tasks/year3-demo-workspace/Lecture03-ClassBMI/main.cpp
+++ Tasks/year3-demo-workspace/Lecture03-ClassBMI/main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    //This looks the same?
+    //Static allocation
     CHuman person1(35, 80.0, 1.81);
     
     //Display
     person1.display();
     
     //Designated initialiser
-    CHuman person2 = CHuman(45, 97.0, 1.9);
+    CHuman person2(45, 97.0, 1.9);
     cout << person2 << endl;
     
     //Update
@@ -26,5 +26,23 @@ int main(int argc, const char * argv[]) {
     
     //Try this
     //person2._height = 0.0;
+
+    //Dynamic Memory Allocation
+    CHuman* person3 = new CHuman(50, 85.0, 1.9);
+    if (person3 == nullptr) {
+        cout << "Failed to allocate memory";
+        return -1;
+    }
+
+    //Invoke a member function
+    person3->display();
+
+    //Free up memory
+    delete person3;
+    person3 = nullptr;
+
+    //Error - do not do this!
+    //person3->display();
+
     return 0;
 }
