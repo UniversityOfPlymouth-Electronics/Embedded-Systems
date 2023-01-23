a805d686cd31a6a0b4e8c95c9b7e481ec731959f
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2023-01-23 11:05:10 +0000

    encapsulated in a class

--- Hardware/Environmental-Sensors/DHT20/dht20.h
+++ Hardware/Environmental-Sensors/DHT20/dht20.h
@@ -1,16 +1,145 @@
 #pragma once
-#include "PinNames.h"
 #include "mbed.h"
+#include <functional>
+
+/*
+    //Call back to write status messages in the terminal
+    auto logger = [](const char* m) {
+        cout << m << endl;
+    };
+    //Create instance of DHT20 sensor
+    DHT20 sensor(ARDUINO_UNO_D14, ARDUINO_UNO_D15, logger);
+
+    //Test
+    while (true) {
+        DHT20::EnvData e = sensor.readTemperatureAndHumidity();
+        printf("Temperature = %8.3f\tHumidity = %8.3f\n\r", e.temperature, e.Humidity);
+        wait_us(1000000);
+    }
+*/
 
 class DHT20 {
+public:
+  typedef struct {
+      double temperature;
+      double Humidity;
+  } EnvData;
+  typedef function<void(const char*)> CALLBACK_t;
+
 private:
-    I2C _sensor;
+  I2C _sensor;
+  DigitalOut _led;
+  int _status;
+  const char statusCommand = 0x71;
+  const char resetCommand = 0xBA;
+  const int SHA_I2C_ADDR = 0b01110000;
+  char byteArray[7];
+  double _humidity;
+  double _temperature;
+  CALLBACK_t _msg; //Call back
+
+private:
+  void critErr() {
+      while (true) {
+          _msg("Critical Error");
+          _led = !_led;
+          wait_us(125000);
+      }
+  }
+  void checkStatus() {
+      if (_status > 0) {
+        error("NACK: Code = %X\n\r", _status);
+        critErr();
+      }    
+  }
+
 
 public:
-    DHT20(PinName SDA = ARDUINO_UNO_D14, PinName SCL = ARDUINO_UNO_D15) : _sensor(SDA,SCL)
-    {
-        
+  DHT20(PinName SDA = ARDUINO_UNO_D14, PinName SCL = ARDUINO_UNO_D15, CALLBACK_t msg = {}) : _sensor(SDA, SCL), _led(LED1), _msg(msg)
+  {
+    _status = 0;
+
+    // Give sensor 100ms to start up
+    _msg("Waiting 100ms");
+    wait_us(100000);
+    // Setting Clock Frequency
+    _msg("Setting clock frequency");
+    _sensor.frequency(100000); // 100kHz
+    // Give sensor 500ms to start up
+    _msg("Waiting 500ms");
+    wait_us(500000);
+
+    //Soft reset
+    _msg("Soft Reset");
+    _status = _sensor.write(SHA_I2C_ADDR, &resetCommand, 1);
+    checkStatus();
+    _msg("Waiting 500ms");
+    wait_us(500000);
+
+    // Startup Routine
+    _msg("Checking Status Command");
+    _status = _sensor.write(SHA_I2C_ADDR, &statusCommand, 1);
+    checkStatus();
+
+    // Read Status
+    _msg("Read Back Status");
+    _status = _sensor.read(SHA_I2C_ADDR, byteArray, 1);
+    checkStatus();
+    if (byteArray[0] != 0x18) {
+      _msg("Sensor does not return 0x18 - recalibrate?");
+    } else {
+      _msg("Sensor looks good! Received 0x18");  
     }
 
-};
+    // 10ms wait
+    _msg("Delay 10ms");
+    wait_us(10000);
+  }
 
+  EnvData readTemperatureAndHumidity() {
+    // ***Read Sensor Data***
+    _msg("Start Measurement Command");
+    const char readCmd[] = {0xAC, 0x33, 0x00};
+    _status = _sensor.write(SHA_I2C_ADDR, readCmd, 3);
+    checkStatus();
+
+    // Check if ready
+    do {
+      _msg("Checking...");
+      wait_us(80000); // 80ms wait (for sensors to be ready)
+      _status = _sensor.read(SHA_I2C_ADDR, byteArray, 1);
+      checkStatus();
+    } while ((byteArray[0] & 0b01000000) != 0);
+
+    // Grab 6 bytes of data (6 data, 1 CRC)
+    _msg("Reading 6 bytes of data + 1 byte CRC");
+    _status = _sensor.read(SHA_I2C_ADDR, byteArray, 7);
+    checkStatus();
+
+    // Extract Temp and Humidity
+    uint32_t h;
+    h = byteArray[1];
+    h = (h << 8) | byteArray[2];
+    h = (h << 4) | (byteArray[3] >> 4);
+
+    uint32_t t;
+    t = byteArray[3] & 0x0F;
+    t = (t << 8) | byteArray[4];
+    t = (t << 8) | byteArray[5];
+
+    _humidity = 100.0 * (double)h / (double)(1 << 20);
+    _temperature = (200.0 * (double)t / (double)(1 << 20)) - 50.0;
+
+    EnvData res = {.temperature = _temperature, .Humidity = _humidity};
+    return res;
+  }
+
+  double getTemperature() {
+      return _temperature;
+  }
+
+  double getHumidity() {
+      return _humidity;
+  }
+
+};
--- Hardware/Environmental-Sensors/DHT20/main.cpp
+++ Hardware/Environmental-Sensors/DHT20/main.cpp
@@ -2,101 +2,24 @@
  * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
-
-#include "PinNames.h"
 #include "mbed.h"
-#include "I2C.h"
 #include <iostream>
+#include "dht20.h"
 using namespace std;
-
-#define WAIT_TIME_MS 500 
-DigitalOut led1(LED1);
-
-//I2C interface
-I2C dht20(ARDUINO_UNO_D14, ARDUINO_UNO_D15);   //SDA,SCL : I2C1 on the Arduino interface
-const int SHA_I2C_ADDR = 0b01110000;
 
 int main()
 {
-    int status = 0;
-    auto checkStatus = [&](){
-        if (status == 0) {
-            cout << "ACK" << endl;
-        } else {
-            printf("NACK: Code = %X\n\r", status);
-        }
+    //Call back to write status messages in the terminal
+    auto logger = [](const char* m) {
+        cout << m << endl;
     };
+    //Create instance of DHT20 sensor
+    DHT20 sensor(ARDUINO_UNO_D14, ARDUINO_UNO_D15, logger);
 
-    I2C dht20(ARDUINO_UNO_D14, ARDUINO_UNO_D15);   //SDA,SCL : I2C1 on the Arduino interface  
-    //Give sensor 100ms to start up
-    wait_us(100000);  
-    cout << "Setting Clock Frequency" << endl;
-    dht20.frequency(100000); //100kHz
-    //Give sensor 100ms to start up
-    wait_us(100000);
-
-    cout << "Startup Routine" << endl;
-    const char statusCommand = 0x71;
-    status = dht20.write(SHA_I2C_ADDR, &statusCommand, 1);
-    checkStatus();
-
-    //Generic array of bytes
-    char byteArray[7];
-    
-    //Read Status
-    status = dht20.read(SHA_I2C_ADDR, byteArray, 1);
-    checkStatus();
-    printf("Startup Status: %X\n\r", byteArray[0]);
-    if (byteArray[0] != 0x18) {
-        printf("Please recalibrate\n\r");
-    } else {
-        printf("Device calibration OK\n\r");
-    }
-
-    //10ms wait
-    wait_us(10000);
-
-    // ***Read Sensor Data***
-    printf("Attempting to read env data\n\r");
-    const char readCmd[] = {0xAC, 0x33, 0x00};
-    status = dht20.write(SHA_I2C_ADDR, readCmd, 3);
-    checkStatus();
-     
-    //Check if ready
-    do {
-        printf("Ready?\n\r");
-        wait_us(80000); //80ms wait (for sensors to be ready) 
-        status = dht20.read(SHA_I2C_ADDR, byteArray, 1);
-        checkStatus();
-    } while ((byteArray[0] & 0b01000000) != 0);
-    
-    //Grab 6 bytes of data (6 data, 1 CRC)
-    printf("Reading 6 bytes of data + 1 byte CRC\n\r");
-    status = dht20.read(SHA_I2C_ADDR, byteArray, 7);
-    checkStatus();
-
-    //Extract Temp and Humidity
-    for (unsigned int n=0; n<7; n++) {
-        printf("X[%u] = %X\n\r",n,byteArray[n]);
-    }
-    uint32_t h;
-    h  = ((uint32_t)byteArray[1] << 12);
-    h |= ((uint32_t)byteArray[2] << 4);
-    h |= ((uint32_t)byteArray[3] >> 4);
-    
-    uint32_t t;
-    t  = ((uint32_t)byteArray[3]) & 0x0F; t <<= 16;
-    t |= ((uint32_t)byteArray[4] << 8);
-    t |= (uint32_t)byteArray[5];
-
-    double humidity = 100.0*(double)h / (double)(1<<20);
-    double temp = (200.0 * (double)t / (double)(1<<20)) - 50.0;
-
-    //Done
-    printf("temp = %f\n\rhumid=%f\n\r",temp, humidity); 
-    while (true)
-    {
-        led1 = !led1;
-        thread_sleep_for(WAIT_TIME_MS);
+    //Test
+    while (true) {
+        DHT20::EnvData e = sensor.readTemperatureAndHumidity();
+        printf("Temperature = %8.3f\tHumidity = %8.3f\n\r", e.temperature, e.Humidity);
+        wait_us(1000000);
     }
 }
