1e2a8c22a32e66ec3b06395a784dd311e0ad5b13
NobelO <nobelosazuwa17@gmail.com>
2023-09-28 15:20:12 +0100

    update made on 28.9.23

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -2,6 +2,8 @@
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
@@ -20,12 +22,30 @@ int main()
         greenLED = 1; 
 
         // Wait 0.2 second (1 million microseconds)
-        wait_us(100000); 
+        wait_us(10000); 
 
         // Turn LED OFF
         greenLED = 0;
 
         // Wait 0.2 second
-        wait_us(100000); 
+        wait_us(50000);
+
+
+        blueLED = 1;
+
+        wait_us(10000);
+
+        blueLED = 0;
+
+        wait_us(50000);
+
+
+        redLED = 1;
+
+        wait_us(10000);
+
+        redLED = 0;
+
+        wait_us(50000);
     }
 }
