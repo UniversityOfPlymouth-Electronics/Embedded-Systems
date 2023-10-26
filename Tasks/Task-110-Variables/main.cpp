f7e50ea239b2a917ca3f68347e9d04b1bb3a0250
Magdalena Ruzicka <116365860+Magdalena-sisi@users.noreply.github.com>
2023-10-26 14:52:31 +0100

    110 - variables

--- Tasks/Task-110-Variables/main.cpp
+++ Tasks/Task-110-Variables/main.cpp
@@ -27,7 +27,7 @@ int main()
     int count = 10; // initialse count to value 10
     printf("count = %d\n", count);
 
-    // Add one to count
+    // Add two to count
     count = count + 2;
     printf("New value of count = %d\n", count); // add 2 to the count
 
@@ -36,54 +36,96 @@ int main()
     character = 'A';
    // character = 65;
 
-    //printf("The character %c has the ASCII code %d\n", character, character);
-    printf(R"(The character %c = %d\n)",character, character); // a=65
+   // printf("The character %c has the ASCII code %d\n", character, character);
+   // printf(R"(The character %c = %d\n)",character, character); //a wass declere a=65
+
+    //char character;
+   // character = 'Z'; //char Z-ASCII code 90
+   // printf("The character %c has the ASCII code %d\n", character, character);
+    //char character;
+   // character = 'a'; //char Z-ASCII code 97
+   // printf("The character %c has the ASCII code %d\n", character, character);
 
         
 
     // Short
-   /* short shortCount = 32766;
+    short shortCount = 32766;
+    printf("The value of shortCount is %hd\n", shortCount); // h = half
+    shortCount = shortCount + 1; // value of short become negative number plus 2 "overflow start con from - to +"
+    printf(R"(add 1 and shortCount is %hd)", shortCount); 
+
+    /* short shortCount = 32766; // pluse 2 
     printf("The value of shortCount is %hd\n", shortCount); // h = half
     shortCount = shortCount + 2; // value of short become negative number plus 2
-    printf(R"(add 1 and shortCount is %hd)", shortCount);*/
-
-      // unsigned
-    /* unsigned shortCount = 32766; // change the date type the value is positive
-    printf("The value of shortCount is %u\n", shortCount); // change %hd for %u
-    shortCount = shortCount + 2; // value of short become negative number plus 2
-    printf(R"(add 1 and shortCount is %u)", shortCount); // change %hd for %u*/
-
-     // int
-    unsigned intCount = 32766; // using unsigned intCount give the same result as  unsigned shortCount
-    printf(R"(The value of shortCount is %u)",intCount);
-    intCount = intCount + 2; 
-    printf(R"(add 1 and shortCount is %u)", intCount); 
+    printf(R"(add 1 and shortCount is %hd)", shortCount); */
 
 
-    
 
-    // Long long
-    long long NN = 0x12345678ABCD0001LL; //Literal in HEX
-    
+   
+     
+
+     // Long long
+     long long NN = 0x12345678ABCD0001LL; // Literal in HEX
      printf("A very large number %lld\n", NN);
     // printf("A very large number %d\n", NN);//error
-    printf("A very large number in hex %llX\n", NN);
+     printf("A very large number in hex %llX\n", NN);
 
-    
 
+      /*long long NN = 0x12345678ABCD0001LL; // change the place holders from lld to d logical error
+     printf("A very large number %d\n", NN); // error format specifies type unsigned int but argument has type lon long
+    // printf("A very large number %d\n", NN);//error
+     printf("A very large number in hex %X\n", NN); */
+
+
+  
     // unsigned
-    unsigned short p = 1; //16 bit
+    /* unsigned short p = 1; //16 bit and underflow back to maximum value = 65535
     printf("unsigned int p = %u\n", p);
     p = p - 2;
-    printf("Subtract 2. Now unsigned int p = %u\n", p);
+    printf("Subtract 2. Now unsigned int p = %u\n", p); */
 
-    // float 
-    float pi_float = 3.1415926536;
-    printf("The value of pi is approximately %f\n", pi_float);
 
-    // double
-    double pi_double = 3.1415926536l;
-    printf("The value of pi is approximately %lf\n", pi_double);
+   // short p
+    short p = 1; //16 bit
+    printf("short  int p = %d\n", p); // p = 1
+    p = p - 2;
+    printf("Subtract 2. Now short int p = %d\n", p); // p = -1
+
+ // float:standard fractional number type
+
+    /*float pi_float = 3.1415926536;
+    printf("The value of pi is approximately %f\n", pi_float); */
+
+    /*float pi_float = 3.1415926536;
+    printf("The value of pi is approximately %10f\n", pi_float); // change %f to %10f*/
+
+
+     float pi_float = 3.1415926536;
+    printf("The value of pi is approximately %20f\n", pi_float); // change o %10f to 20
+
+
+    // double:double precision.
+
+    /*double pi_double = 3.1415926536l;
+     printf("The value of pi is approximately %lf\n", pi_double);*/
+
+
+   /* double pi_double = 3.1415926536l;
+    printf("The value of pi is approximately %10lf\n", pi_double);  */// change %lf to %.10lf 
+
+ double pi_double = 3.1415926536l;
+    printf("The value of pi is approximately %20lf\n", pi_double); // change %.10lf to 20
+
+
+
+
+
+
+
+
+
+
+
 
     // Data type sizes (in bytes)
     printf("Size of a char is %d bytes\n", sizeof(char));
