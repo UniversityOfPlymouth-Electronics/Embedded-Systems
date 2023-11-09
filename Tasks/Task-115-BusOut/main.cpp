111f63b66e2776eb7de21edf616f1e75fbd5bbee
Magdalena Ruzicka <116365860+Magdalena-sisi@users.noreply.github.com>
2023-11-09 14:56:54 +0000

    115 task
    BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);

--- Tasks/Task-115-BusOut/main.cpp
+++ Tasks/Task-115-BusOut/main.cpp
@@ -6,18 +6,19 @@
 // DigitalOut yellowLED(PC_3,1);
 // DigitalOut greenLED(PC_6,1);
 
-BusOut lights(PC_2, PC_3, PC_6);
+BusOut lights (PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 //BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 
 int main()
 {
     //All OFF
-    lights = 0b000;
+    //lights = 0b000;
+    lights = 0b111111;
 
     while (true)
     {
         int count = 0;
-        while (count <= 7) 
+        while (count <= 63) // 1= red, 2=red and yellow, 3=red,yellow anfd red and yellow together,4=red, yellow,red, green 5=red,yellow, yell-red,green, gre-red
         {
             printf("count = %d\n", count);
             lights = count;
